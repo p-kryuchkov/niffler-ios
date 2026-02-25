2abbeeb96bf19ab7d97fafdaf7793c110dc11d10
Pavel Kryuchkov <krutestkru@gmail.com>
2026-02-25 23:20:38 +0300

    выполнил дз

---
+++ Niffler/NifflerUITests/Test/RegisterUITests.swift
@@ -0,0 +1,17 @@
+import XCTest
+
+final class RegisterUITests: TestCase {
+    
+    func test_RegisterSuccess() throws {
+        
+        launchAppWithoutLogin()
+
+        // Act
+        loginPage.pressRegisterButton()
+        registerPage.input(login: "TestDefaultUser10", password: "12345")
+        
+        // Assert
+        loginPage.assertIsUsernameEquals(expected: "TestDefaultUser10")
+        loginPage.assertIsPasswordNotNull()
+    }
+}
